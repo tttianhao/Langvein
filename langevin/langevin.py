01a70b44e6b6a4da9ca171e6e0e7fc0ee217ef7f
Tianhao Yu <Tianhao@Tianhaos-MacBook-Pro.local>
2018-09-28 11:08:38 -0400

    add accerlation and check wall condition

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -47,24 +47,58 @@ def randomForceGenerator(temperature,dampingCoef,kB =1,delta=1):
     mu = 0
     var = 2*temperature*dampingCoef*kB*delta
     sigma = np.sqrt(var)
+    # draw a random number from a normal distribution with mean=0 and std=sqrt(var)
     Xi = np.random.normal(mu,sigma)
     return Xi
 
 def eulerIntegration(timeStep,totalTime,initialVelocity,dampingCoef,temperature):
     '''
-    This function uses euler method to calculate 
+    This function uses euler method to calculate
+
+    Args:
+        timeStep: the time interval between two times
+        totalTime: the total runtime of the integration
+        initialVelocity: the input velocity at t=0
+        dampingCoef: damping coefficient
+        temperature: the input temperature
+    
+    returns:
+        time: time profile 
+        velocity: velocity at time t
+        accerlation: the derivative of velocity at time t
     '''
     n = totalTime/timeStep+1
     time = np.linspace(0,totalTime,n)
+    #initialize velocity and accerlation
     velocity = np.zeros(n)
     accerlation = np.zeros(n)
+    #set initial velocity and accerlation
     velocity[0] = initialVelocity
-    accerlation[0] = dragForce(dampingCoef,velocity[0])+randomForceGenerator(temperature,dampingCoef)
+    accerlation[0] = accerlationCalculator(dampingCoef,velocity[0],temperature)
     for i in range(1,n):
         randomForce = randomForceGenerator(temperature,dampingCoef)
+        #apply euler equation to estimate y(i) from y(i-1)
+        #equation used is : y_i = dx(f(y_i-1,x_i-1)) + y_i-1
         velocity[i] = timeStep*(dragForce(dampingCoef,velocity[i-1])+randomForce)+velocity[i-1]
-        accerlation[i] = dragForce(dampingCoef,velocity[i])+randomForce
+        accerlation[i] = accerlationCalculator(dampingCoef,velocity[i-1],temperature)
     return time,velocity,accerlation
+
+def accerlationCalculator(dampingCoef,velocity,temperature):
+    accerlation = dragForce(dampingCoef,velocity)+randomForceGenerator(temperature,dampingCoef)
+    return accerlation
+
+def checkWall(position):
+    '''
+    This function checks if the position of the particle is still in the two walls
+    
+    returns:
+        true if particle in the wall and false if not.
+    '''
+
+    if position > 0 and position < 5:
+        return True
+    else:
+        return False
 
 def main(status):
     xi = randomForceGenerator(status.temperature,status.dampingCoef)
