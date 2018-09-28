22f0845e7e49ad20049465aff0a9175a74fd1bfc
Tianhao Yu <Tianhao@Tianhaos-MacBook-Pro.local>
2018-09-28 12:10:39 -0400

    refine euler integration

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -51,11 +51,12 @@ def randomForceGenerator(temperature,dampingCoef,kB =1,delta=1):
     Xi = np.random.normal(mu,sigma)
     return Xi
 
-def eulerIntegration(timeStep,totalTime,initialVelocity,dampingCoef,temperature):
+def eulerIntegration(initialposition,timeStep,totalTime,initialVelocity,dampingCoef,temperature):
     '''
     This function uses euler method to calculate
 
     Args:
+        initialpositon: the input position at t=0
         timeStep: the time interval between two times
         totalTime: the total runtime of the integration
         initialVelocity: the input velocity at t=0
@@ -66,26 +67,34 @@ def eulerIntegration(timeStep,totalTime,initialVelocity,dampingCoef,temperature)
         time: time profile 
         velocity: velocity at time t
         accerlation: the derivative of velocity at time t
+        position: the position of particle at time t
     '''
-    n = totalTime/timeStep+1
+    n = int(totalTime/timeStep+1)
     time = np.linspace(0,totalTime,n)
     #initialize velocity and accerlation
     velocity = np.zeros(n)
     accerlation = np.zeros(n)
+    position = np.zeros(n)
+    #_position = np.zeros(n)
     #set initial velocity and accerlation
     velocity[0] = initialVelocity
-    accerlation[0] = accerlationCalculator(dampingCoef,velocity[0],temperature)
+    accerlation[0] = -dampingCoef*velocity[0] + randomForceGenerator(temperature,dampingCoef)
+    position[0] = initialposition
+    #_position[0] = initialposition
     for i in range(1,n):
         randomForce = randomForceGenerator(temperature,dampingCoef)
         #apply euler equation to estimate y(i) from y(i-1)
         #equation used is : y_i = dx(f(y_i-1,x_i-1)) + y_i-1
         velocity[i] = timeStep*(dragForce(dampingCoef,velocity[i-1])+randomForce)+velocity[i-1]
-        accerlation[i] = accerlationCalculator(dampingCoef,velocity[i-1],temperature)
-    return time,velocity,accerlation
+        accerlation[i] = -dampingCoef*velocity[i] + randomForce
+        #use equation x = x + dt*v
+        position[i] = position[i-1] + timeStep*velocity[i-1]
+        #_position[i] = _position[i-1] + velocity[i-1]*timeStep+0.5*accerlation[i-1]*timeStep**2
+         # check if the particle hits the wall
+        if not checkWall(position[i]):
+            break
+    return time[0:i+1],velocity[0:i+1],accerlation[0:i+1],position[0:i+1]#,_position
 
-def accerlationCalculator(dampingCoef,velocity,temperature):
-    accerlation = dragForce(dampingCoef,velocity)+randomForceGenerator(temperature,dampingCoef)
-    return accerlation
 
 def checkWall(position):
     '''
@@ -95,21 +104,24 @@ def checkWall(position):
         true if particle in the wall and false if not.
     '''
 
-    if position > 0 and position < 5:
+    if position > -50 and position < 50:
         return True
     else:
         return False
 
 def main(status):
-    xi = randomForceGenerator(status.temperature,status.dampingCoef)
-    print('The random force Xi is {}'.format(xi))
-    time,velocity,accerlation = eulerIntegration(status.timeStep,status.totalTime,status.velocity,status.dampingCoef,status.temperature)
+    time,velocity,accerlation,position = eulerIntegration(status.postion,status.timeStep,status.totalTime,status.velocity,status.dampingCoef,status.temperature)
     plt.plot(time,velocity,label='velocity')
     plt.plot(time,accerlation,label='accerlation')
     plt.legend()
     plt.show()
+    plt.plot(time,position,label='position')
+    #plt.plot(time,_position,label='_pos')
+    plt.legend()
+    plt.show()
+    print(time[-1],position[-1],velocity[-1])
 
 
 if __name__ == '__main__':
-    testCase = status(2,1,298,0.1,1,100)
+    testCase = status(0,0,300,0.01,0.01,1000)
     main(testCase)
