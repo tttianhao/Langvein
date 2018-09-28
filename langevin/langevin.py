f5d37856f52d80c624e92ccabd06bd56373b6f06
Tianhao Yu <Tianhao@Tianhaos-MacBook-Pro.local>
2018-09-28 09:42:41 -0400

    add unit test of random force function

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -4,64 +4,51 @@
 
 import numpy as np
 import scipy.stats as ss
+import matplotlib.pyplot as plt
 
-class initial:
+class status:
+
     '''This class initialize the initial status of the particle'''
-    def __init__(self,position,velocity, temperature, dampingCoef,timeStep,totalTime):
+
+    def __init__(self,position,velocity, temperature, dampingCoef,timeStep,totalTime,mass=1):
         self.postion = position
         self.velocity = velocity
         self.temperature = temperature
         self.dampingCoef = dampingCoef
         self.timeStep = timeStep
         self.totalTime = totalTime
+        self.mass = mass
 
 class dragForce:
     def __init__(self):
         pass
 
-class randomForce:
-    @staticmethod
-    def variance(temperature,dampingCoef,kB=1,delta=1):
 
-        '''
-        This function calculates the variance of random force
 
-        Args: 
-            temperature: float
-            dampingCoef: float
-            kB = 1
-            delta = 1
-        
-        return: 
-            variance of the normal distribution
-        '''
+def randomForceGenerator(temperature,dampingCoef,kB =1,delta=1):
 
-        var = 2*temperature*dampingCoef*kB*delta
-        return var
+    '''
+    This function generate a random number from a normal distribution, whose mean is zero and variance is determined by 'variance' function.
 
-    @staticmethod
-    def randomForceGenerator(temperature,dampingCoef):
+    returns:
+        Xi: the random force at a certain instance
+    '''
 
-        '''
-        This function generate a random number from a normal distribution, whose mean is zero and variance is determined by 'variance' function.
-
-        returns:
-            Xi: the random force at a certain instance
-        '''
-
-        mu = 0
-        sigma = np.sqrt(randomForce.variance(temperature,dampingCoef))
-        Xi = np.random.normal(mu,sigma)
-        return Xi
+    mu = 0
+    var = 2*temperature*dampingCoef*kB*delta
+    sigma = np.sqrt(var)
+    Xi = np.random.normal(mu,sigma)
+    return Xi
 
 class langvein:
     def __init__(self):
         pass
 
 def main(initial):
-    xi = randomForce.randomForceGenerator(initial.temperature,initial.dampingCoef)
+    xi = randomForceGenerator(initial.temperature,initial.dampingCoef)
     print('The random force Xi is {}'.format(xi))
+    
 
 if __name__ == '__main__':
-    testCase = initial(2,1,298,1,2,60)
+    testCase = status(2,1,298,1,2,60)
     main(testCase)
