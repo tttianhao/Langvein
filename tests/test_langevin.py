a830b00850d36921c4669ea7dad898163c8d22d1
Tianhao Yu <Tianhao@dhcp-10-5-138-113.wireless.rochester.edu>
2018-09-29 13:21:46 -0400

    add test on main function

--- tests/test_langevin.py
+++ tests/test_langevin.py
@@ -5,7 +5,7 @@
 import numpy as np
 import unittest
 import pytest
-from langevin import *
+from langevin import status,dragForce,randomForceGenerator,eulerIntegration,checkWall,outPut,main
 import scipy.stats as ss
 import random
 
@@ -92,6 +92,10 @@ class Testworkshop(unittest.TestCase):
         with open('Langvein_dynamics_output.txt') as f:
             first_line = f.readline()
             self.assertEquals(first_line,'index  time  position  velocity \n')
+
+    def test_main(self):
+        defaultCase = status(0,0,300,0.1,0.1,1000)
+        main(defaultCase)
 
 if __name__ == '__main__':
     unittest.main()
